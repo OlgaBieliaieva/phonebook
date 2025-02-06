6ec5b2063af7dce05caee0876b6a58731393d715
OlgaBieliaieva <belyaevaolya0@gmail.com>
2025-02-06 13:01:36 +0200

    updated server path

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const workspaceApiClient = axios.create({
-  // baseURL: "https://workspace-api-production.up.railway.app/api/",
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "https://workspace-api-production.up.railway.app/api/",
+  // baseURL: "http://localhost:8000/api/",
   withCredentials: true,
 });
