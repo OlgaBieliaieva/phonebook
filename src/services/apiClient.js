e2073de13d4cd98232685927766664e86073094a
Olha Shapoval <belyaevaolya0@gmail.com>
2025-11-12 16:58:37 +0200

    updated api url

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const workspaceApiClient = axios.create({
-  baseURL: "https://workspace-api-73du.onrender.com/api/",
+  baseURL: "https://workspace-api-nthp.onrender.com/api/",
   // "https://workspace-api-production.up.railway.app/api/",
   // baseURL: "http://localhost:8000/api/",
   withCredentials: true,
